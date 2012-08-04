512b09cca9e1caa8f2d915a6d76d221a28602ed4
Roger Chen <rogerthechen@gmail.com>
2012-08-04 09:24:01 -0700

    MarioKartPlayer

--- RemoteBluetoothServer/src/com/luugiathuy/apps/remotebluetooth/MarioKartPlayer.java
+++ RemoteBluetoothServer/src/com/luugiathuy/apps/remotebluetooth/MarioKartPlayer.java
@@ -3,16 +3,20 @@ import java.util.*;
 import java.awt.AWTException;
 
 public class MarioKartPlayer extends Player {
-	private static final float xTHRES = (float) 1.0;
-	private static final float yTHRES = (float) 1.0;
-	private static final float zTHRES = (float) 1.0;
+	private static final float xTHRES = (float) 0.01;
+	private static final float yTHRES = (float) 0.01;
+	private static final float zTHRES = (float) 0.01;
 	
 	public MarioKartPlayer(String bid, String name, int playerNum) throws AWTException {
       super(bid, name, playerNum);
   }
 
 	protected void updateAccl(float newX, float newY, float newZ) {
-    if (acclCallb){
+    System.out.println("asdf");
+		
+		if (acclCallb){
+    	
+    	System.out.println("ghij");
 
 	    float delX = newX-accl_offsetX;
 	    float delY = newY-accl_offsetY;
@@ -21,23 +25,26 @@ public class MarioKartPlayer extends Player {
 
         if (delY > 0){
           System.out.println(RIGHT);
+          player.keyRelease(LEFT);
           player.keyPress(RIGHT);
-          player.keyRelease(RIGHT);
         }else{
           System.out.println(LEFT);
+          player.keyRelease(RIGHT);
           player.keyPress(LEFT);
-          player.keyRelease(LEFT);
         }
       }
       if (Math.abs(delX) > xTHRES){
         if (delX > 0){
           System.out.println(BUTTONB);
+          player.keyRelease(BUTTONA);
+
           player.keyPress(BUTTONB);
-          player.keyRelease(BUTTONB);
         }else{
           System.out.println(BUTTONA);
+          player.keyRelease(BUTTONB);
+
           player.keyPress(BUTTONA);
-          player.keyRelease(BUTTONA);
+          
         }
       }
 			//if (Math.abs(delZ) > zTHRES) {
