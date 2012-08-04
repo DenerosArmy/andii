fbf3876cb6a0e99717686dd5316409ba1267cfb0
Jian Wei Leong <jian@Jians-MacBook-Air.local>
2012-08-04 06:23:13 -0700

    updated player classes

--- RemoteBluetoothServer/src/com/luugiathuy/apps/remotebluetooth/MarioKartPlayer.java
+++ RemoteBluetoothServer/src/com/luugiathuy/apps/remotebluetooth/MarioKartPlayer.java
@@ -6,31 +6,19 @@ public class MarioKartPlayer extends Player {
 	private static final float xTHRES = (float) 1.0;
 	private static final float yTHRES = (float) 1.0;
 	private static final float zTHRES = (float) 1.0;
-	private String bid;
-	private String name;
-	private float offsetX;
-	private float offsetY;
-	private float offsetZ;
 	private float lastX;
 	private float lastY;
 	private float lastZ;
 	
-	public MarioKartPlayer(String bid, String name, float offsetX, float offsetY, float offsetZ) throws AWTException {
-      super();
-	    this.bid = bid;		//bluetooth ID
-	    this.name = name;
-	    this.offsetX = offsetX;
-	    this.offsetY = offsetY;
-	    this.offsetZ = offsetZ;
-	    this.lastX = offsetX;
-	    this.lastY = offsetY;
-	    this.lastZ = offsetZ;
+	public MarioKartPlayer(String bid, String name, int playerNum) throws AWTException {
+      super(bid, name, player);
   }
-	
-	protected void issueCommand(float newX, float newY, float newZ, String[] buttons) {
-	    float delX = newX-offsetX;
-	    float delY = newY-offsetY;
-	    float delZ = newZ-offsetZ;
+
+	protected void updateGyro(float newX, float newY, float newZ) {
+    if (gyroCallb){
+	    float delX = newX-gyro_offsetX;
+	    float delY = newY-gyro_offsetY;
+	    float delZ = newZ-gyro_offsetZ;
       for (String button:buttons){
         player.keyPress(Integer.parseInt(button));
       }
@@ -38,18 +26,22 @@ public class MarioKartPlayer extends Player {
         if (delY > 0){
           System.out.println(Player.RIGHT);
           player.keyPress(Player.RIGHT);
+          player.keyRelease(Player.RIGHT);
         }else{
           System.out.println(Player.LEFT);
           player.keyPress(Player.LEFT);
+          player.keyRelease(Player.LEFT);
         }
       }
       if (Math.abs(delX) > xTHRES){
         if (delX > 0){
           System.out.println(Player.BUTTONB);
           player.keyPress(Player.BUTTONB);
+          player.keyRelease(Player.BUTTONB);
         }else{
           System.out.println(Player.BUTTONA);
           player.keyPress(Player.BUTTONA);
+          player.keyRelease(Player.BUTTONA);
         }
       }
 			//if (Math.abs(delZ) > zTHRES) {
@@ -70,5 +62,6 @@ public class MarioKartPlayer extends Player {
 				//}
 	    //}
 	  }
+  }
 	
 }
--- RemoteBluetoothServer/src/com/luugiathuy/apps/remotebluetooth/Player.java
+++ RemoteBluetoothServer/src/com/luugiathuy/apps/remotebluetooth/Player.java
@@ -16,9 +16,19 @@ public abstract class Player {
   protected static final int[] CONFIG2 = {KeyEvent.VK_8,KeyEvent.VK_9,KeyEvent.VK_0,KeyEvent.VK_Q,KeyEvent.VK_W,KeyEvent.VK_E,KeyEvent.VK_M};
   protected static final int[] CONFIG3 = {KeyEvent.VK_T,KeyEvent.VK_Y,KeyEvent.VK_U,KeyEvent.VK_I,KeyEvent.VK_O,KeyEvent.VK_P,KeyEvent.VK_N};
   protected static final int[] CONFIG4 = {KeyEvent.VK_V,KeyEvent.VK_D,KeyEvent.VK_F,KeyEvent.VK_G,KeyEvent.VK_H,KeyEvent.VK_J,KeyEvent.VK_K};
+	protected String bid;
+	protected String name;
+	private float gyro_offsetX;
+	private float gyro_offsetY;
+	private float gyro_offsetZ;
+  private float accl_offsetX;
+	private float accl_offsetY;
+	private float accl_offsetZ;
+  private boolean gyroCallb = false;
+  private boolean acclCallb = false;
   protected Robot player;
 
-  public Player(int playerNum) throws AWTException{
+  public Player(String bid, String name, int playerNum) throws AWTException{
     this.player = new Robot();
     int[] config;
     switch(playerNum){
@@ -35,15 +45,38 @@ public abstract class Player {
         config = CONFIG4;
         break;
     }
-    UP = config[0];
-    DOWN = config[1];
-    LEFT = config[2];
-    RIGHT = config[3];
-    BUTTONA = config[4];
-    BUTTONB = config[5];
-    START = config[6];
+    this.UP = config[0];
+    this.DOWN = config[1];
+    this.LEFT = config[2];
+    this.RIGHT = config[3];
+    this.BUTTONA = config[4];
+    this.BUTTONB = config[5];
+    this.START = config[6];
+    this.bid = bid;
+    this.name = name;
   }
 
-	protected void issueCommand(float newX, float newY, float newZ, String[] buttons) {
+  protected void setGyroOffset(float offsetX, float offsetY, float offsetZ){
+    this.gyro_offsetX = gyro_offsetX;
+    this.gyro_offsetY = gyro_offsetY;
+    this.gyro_offsetZ = gyro_offsetZ;
+    gyroCallb = true;
   }
+
+  protected void setAcclOffset(float offsetX, float offsetY, float offsetZ){
+    this.accl_offsetX = accl_offsetX;
+    this.accl_offsetY = accl_offsetY;
+    this.accl_offsetZ = accl_offsetZ;
+    acclCallb = true;
+  }
+
+  protected void pressButton(int key){
+    player.keyPress(key);
+    player.keyRelease(key);
+  }
+
+	protected void updateGyro(float newX, float newY, float newZ){}
+
+	protected void updateAccl(float newX, float newY, float newZ){}
+
 }
