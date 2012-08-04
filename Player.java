a0dc83de0d6b2d0f08d35155bd52e5dc6ee7d1e3
Vishal Yadav <vishalyadav@Vishals-MacBook-Pro.local>
2012-08-04 02:38:30 -0700

    fixed player classes

--- Player.java
+++ Player.java
@@ -1,8 +1,12 @@
-public abstract class Player{
+package com.denerosarmy;
+
+import java.awt.event.KeyEvent;
+
+public abstract class Player {
   private static final int UP = KeyEvent.VK_UP;
   private static final int DOWN = KeyEvent.VK_DOWN;
-  private static final int LEFT = KeyEvent.VK_LEFT;
-  private static final int RIGHT = KeyEvent.VK_RIGHT;
+  protected static final int LEFT = KeyEvent.VK_LEFT;
+  protected static final int RIGHT = KeyEvent.VK_RIGHT;
   private static final int BUTTONA = KeyEvent.VK_A;
   private static final int BUTTONB = KeyEvent.VK_B;
   private static final int START = KeyEvent.VK_SHIFT;
