1fc2b1a8e06fb13b9be71fb9a5ebbbe39616e057
Vaishaal <vaishaal@berkeley.edu>
2012-08-04 01:58:11 -0700

    added bluetooth listener

---
+++ src/com/denerosarmy/bluetoothlistener/RemoteBluetoothServer.java
@@ -0,0 +1,11 @@
+package com.denerosarmy.bluetoothlistener;
+
+
+
+public class RemoteBluetoothServer{
+	
+	public static void main(String[] args) {
+		Thread waitThread = new Thread(new WaitThread());
+		waitThread.start();
+	}
+}
